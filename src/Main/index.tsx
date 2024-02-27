df86bf0230b74bec70e7b7bbe749f2b93a3c661a
guantao <1368004939@qq.com>
2024-02-27 19:51:53 +0800

    初始化

---
+++ src/Main/index.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+const Main = () => {
+  return (
+    <div className="main">
+      header
+    </div>
+  )
+}
+
+
+export default Main
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+const Header = () => {
+  return (
+    <div className="header h-4">
+      header
+    </div>
+  )
+}
+
+
+export default Header
