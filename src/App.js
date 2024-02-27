6c9b471af6fb2fb5fad00405549ae5ca58ad0e1a
guantao <1368004939@qq.com>
2024-02-27 20:48:45 +0800

    提交更改

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import './App.css';
 import Main from './Main/index.tsx';
 import Header from './components/Header/index.tsx';
+import React from 'react';
+import { useScroll } from "ahooks"
+
 
 function App() {
+  const scroll = useScroll(document)
+
   return (
-    <div className="App container mx-auto max-w-[1024px]">
-      <Header></Header>
-      <Main></Main>
-    </div>
+    <>
+      <div className={`App`}>
+        <Header className={`${scroll && scroll.top >= 200 ? 'header--sticky': ''}`}></Header>
+        <Main></Main>
+      </div>
+    </>
   );
 }
 
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,14 @@
 import React from "react"
 
+
+
 const Header = () => {
+  // scroll
   return (
-    <div className="header h-4">
-      header
+    <div className={`header`}>
+      <div className="header__inner container h-4 flex justify-between items-center">
+        <div className="header__left"></div>
+      </div>
     </div>
   )
 }
